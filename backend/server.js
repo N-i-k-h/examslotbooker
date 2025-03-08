d8c8859c4521d5a990ed839116b7ed3704ea71f8
N-I-K-H <nikhilkashyap4217.com>
2025-03-08 20:09:59 +0530

    exam slot booker

--- backend/server.js
+++ backend/server.js
@@ -271,5 +271,4 @@ app.use((err, req, res, next) => {
 
 // ✅ Start Server
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`✅ Server running on http://localhost:${PORT}`));
- 
+app.listen(PORT, '0.0.0.0', () => console.log(`✅ Server running on http://localhost:${PORT}`));
